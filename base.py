b140600f5edc9973bd77d2fd160b3a8ce9d4791e
Aaron Mendoza <aaronmg@Mac-mini-de-Aaron.local>
2021-06-09 00:17:15 -0500

    Add class two-Metodos de lista y funcion lambda

---
+++ base.py
@@ -0,0 +1,15 @@
+#LISTAS
+#ESTRUCUTRA DE DATOS EN PYTHON QUE NOS PERMITE ALAMACENAR CUALQUIER TIPO DE VALOR COMO 
+#̦ENTEROS,CADENAS,FUNCIONES HASTA OTRAS LISTAS
+
+##F-STRINGS
+
+nombre="DyNet"
+objetivo="Apoyamos en la proteccion de la infraestructura informatica"
+queHace="Proteccion de seguridad perimetral, seguridad de datos y conectividad"
+
+print(f'''Nombre: {nombre}
+Objetivo: {objetivo}
+Que hace?: {queHace}
+''')
+
